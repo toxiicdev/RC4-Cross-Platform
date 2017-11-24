24a780656aeae705961aba49c5a17541a33bdd11
Giuseppe <toxiicpp@gmail.com>
2017-11-24 15:23:18 +0100

    Create RC4.h

---
+++ RC4.h
@@ -0,0 +1,39 @@
+#ifndef ARC4_H_
+#define ARC4_H_
+
+class RC4
+{
+public:
+	static void encrypt(unsigned char* input, int input_length, unsigned char* output, unsigned char* key, int key_length)
+	{
+		int x, y, j = 0;
+
+		Game::WriteMemory(output, input, input_length);
+
+		unsigned char box[256];
+		for (int i = 0; i < 256; i++)
+			box[i] = i;
+
+		for (int i = 0; i < 256; i++)
+		{
+			j = (key[i % key_length] + box[i] + j) % 256;
+			x = box[i];
+			box[i] = box[j];
+			box[j] = x;
+		}
+
+		for (int i = 0; i < input_length; i++)
+		{
+			y = (i + 1) % 256;
+			j = (box[y] + j) % 256;
+			x = box[y];
+			box[y] = box[j];
+			box[j] = x;
+
+			output[i] ^= box[(box[y] + box[j]) % 256];
+		}
+	}
+};
+
+
+#endif
